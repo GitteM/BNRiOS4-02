e87fb09ea1a639adb277ea8c704d34e22e39ff13
Brigitte Michau <brigitte@fontera.com>
2014-08-01 10:02:16 +0200

    Testing subclasses

--- RandomItems/main.m
+++ RandomItems/main.m
@@ -14,45 +14,20 @@ int main(int argc, const char * argv[])
     
     @autoreleasepool {
         
-        // Create a mutable array object, store its address in items variable
-        
         NSMutableArray *items = [[NSMutableArray alloc]init];
         
-        // Send the message addObject: to the NSMutableArray pointed to
-        // by the variable items, passing a string each time
-        
-        [items addObject:@"One"];
-        [items addObject:@"Two"];
-        [items addObject:@"Three"];
-        
-        // Send another message, insertObject:atIndex:, to that same array object
-        
-        [items insertObject:@"Zero" atIndex:0];
-        
-        // For every item in the items array ...
-        
-        for (NSString *item in items) {
-            
-            // Log the description of item
-            
-            NSLog(@"%@", item);
+        for (int i = 0; i < 10; i++) {
+            BNRItem *item = [BNRItem randomItem];
+            [items addObject:item];
         }
         
-        // Destroy the mutable array object
+        for (BNRItem *item in items) {
+            NSLog(@"%@", item);
+        }
+     
         items = nil;
-        
-        BNRItem *item = [[BNRItem alloc]initWithItemName:@"Red Sofa"
-                                          valueInDollars:100
-                                            serialNumber:@"A1B2C"];
-        NSLog(@"%@", item);
-        
-        BNRItem *itemName = [[BNRItem alloc]initWithItemName:@"Blue Sofa"];
-        NSLog(@"%@", itemName);
-        
-        BNRItem *itemWithNoName = [[BNRItem alloc]init];
-        NSLog(@"%@", itemWithNoName);
-        
     }
+    
     return 0;
 }
 
